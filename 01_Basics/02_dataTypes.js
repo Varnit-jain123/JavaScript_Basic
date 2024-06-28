a59b9ae7a725cab647016d5abd9ae32726e3ebd4
Varnit-jain123 <varnitjaintj@gmail.com>
2024-06-28 12:10:18 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat All js code as newer version
+
+// alert (3+3)
+
+let name="varnit" //string datatype
+let age=18 // number type datatype
+let isLoggedIn = false // boolean type datatype
+let state= null
+
+/* different datatype */
+// number => 2 to power 53 =>range
+// bigint
+// string =>""
+// boolean => true/false
+// null =>stand alone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age)  //number
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
+
